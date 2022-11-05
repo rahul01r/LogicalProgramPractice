f17fbffa89d051e0577752c9a80b55000bf18b98
rahul01r <rrdubeynibm@gmail.com>
2022-11-05 08:37:25 +0530

    [rahul] Add:UC5-CoupanNumber

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -6,7 +6,7 @@
         {
             while (true)
             {
-                Console.WriteLine("Select program \n1.Fibonacci series\n2 Given Number is Perfect or Not\n3.Enter the number to check either its Prime num or not\n4. Reverse Number");
+                Console.WriteLine("Select program \n1.Fibonacci series\n2 Given Number is Perfect or Not\n3.Enter the number to check either its Prime num or not\n4. Reverse Number\n5.CCoupan Number");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -26,6 +26,10 @@
                         ReverseNumber reverseNumber = new ReverseNumber();
                         reverseNumber.RevNumber();
                         break ; 
+                    case 5:
+                        CoupanNumber coupanNumber = new CoupanNumber();
+                        coupanNumber.DistnictCoupanNumber();
+                        break;
                 }
             }
         }
